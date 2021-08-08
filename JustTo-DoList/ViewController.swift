bb1e3dbe16bd95cc1c79a71278fd0e1dc22c8256
Anton Cherkasov <unsaferawpointer@gmail.com>
2021-08-08 10:21:48 +0300

    Initial Commit

---
+++ JustTo-DoList/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  JustTo-DoList
+//
+//  Created by Anton Cherkasov on 08.08.2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
